9b57ca1a9ee6587b6b68fa7eb5b337d4b1ebcea4
toofnf <kaneva.tmr@gmail.com>
2021-10-09 16:16:49 +0300

    add objects

--- homework04/pyvcs/objects.py
+++ homework04/pyvcs/objects.py
@@ -34,29 +34,85 @@ def hash_object(data: bytes, fmt: str, write: bool = False) -> str:
     return hash_sum
 
 
-def resolve_object(obj_name: str, gitdir: pathlib.Path) -> tp.List[str]:
-    # PUT YOUR CODE HERE
-    ...
+def resolve_object(obj_name: str,
+                   gitdir: pathlib.Path) -> tp.List[str]:
+    """
+    Ищет захэшированные файлы
+
+    :version: 0.3.0
+
+    :param obj_name:
+    :param gitdir:
+    :return:
+    """
+    obj_len = len(obj_name)
+
+    if not 5 <= obj_len <= 39:
+        raise Exception(f"Not a valid object name {obj_name}")
+
+    objects_path = gitdir / "objects"
+    folder_name = obj_name[:2]
+
+    base_path = objects_path / folder_name
+    files = []
+
+    for file in base_path.glob("*"):
+        dummy_filename = file.parent.name + file.name
+
+        if obj_name == dummy_filename[:obj_len]:
+            files.append(dummy_filename)
+
+    if not files:
+        raise Exception(f"Not a valid object name {obj_name}")
+    return files
 
 
 def find_object(obj_name: str, gitdir: pathlib.Path) -> str:
-    # PUT YOUR CODE HERE
-    ...
+    pass
 
 
 def read_object(sha: str, gitdir: pathlib.Path) -> tp.Tuple[str, bytes]:
-    # PUT YOUR CODE HERE
-    ...
+    """
+    Зеркальная функция к has_object - декодирует хэшированный файл
+
+    Возвращает формат (blob, commit, e.t.c) и раздекодированное сообщение
+
+    :version: 0.3.0
+
+    :param sha:
+    :param gitdir:
+    :return:
+    """
+    symbol, space = b"\00", b" "
+    folder_name, filename = sha[:2], sha[2:]
+    full_path = gitdir / "objects" / folder_name / filename
+
+    with open(full_path, "rb") as f:
+        obj_data = zlib.decompress(f.read())
+
+    finish = obj_data.find(symbol)
+    header = obj_data[:finish]
+    start = header[:header.find(space)]
+    fmt = start.decode("ascii")
+    data = obj_data[finish+1:]
+    return fmt, data
 
 
 def read_tree(data: bytes) -> tp.List[tp.Tuple[int, str, str]]:
-    # PUT YOUR CODE HERE
-    ...
+    pass
 
 
 def cat_file(obj_name: str, pretty: bool = True) -> None:
-    # PUT YOUR CODE HERE
-    ...
+    """
+    :version: 0.3.0
+
+    :param obj_name:
+    :param pretty:
+    :return:
+    """
+    gitdir = repo_find()
+    fmt, data = read_object(obj_name, gitdir)
+    print(data.decode() if pretty else data)
 
 
 def find_tree_files(tree_sha: str, gitdir: pathlib.Path) -> tp.List[tp.Tuple[str, str]]:
