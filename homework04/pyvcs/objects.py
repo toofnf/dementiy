a96fb0368e22e67ca576e61c1627d0c5b35bba37
toofnf <kaneva.tmr@gmail.com>
2021-10-09 16:11:02 +0300

    add hash_object function

--- homework04/pyvcs/objects.py
+++ homework04/pyvcs/objects.py
@@ -11,8 +11,27 @@ from pyvcs.repo import repo_find
 
 
 def hash_object(data: bytes, fmt: str, write: bool = False) -> str:
-    # PUT YOUR CODE HERE
-    ...
+    """
+    Вычисление хэш-суммы
+
+    :version: 0.2.0
+
+    :param data:
+    :param fmt:
+    :param write:
+    :return:
+    """
+    store = f"{fmt} {len(data)}\0".encode() + data
+    hash_sum = hashlib.sha1(store).hexdigest()
+    if write:
+        folder_name, file_name = hash_sum[:2], hash_sum[2:]
+        obj_dir = repo_find() / "objects" / folder_name
+        if not obj_dir.exists():
+            obj_dir.mkdir()
+
+        with open(obj_dir / file_name, "wb") as f:
+            f.write(zlib.compress(store))
+    return hash_sum
 
 
 def resolve_object(obj_name: str, gitdir: pathlib.Path) -> tp.List[str]:
